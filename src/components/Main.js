dfa0003d44fb7683d4885c8d3222ff5ee3dc4b98
Kasun Gunawardana <kasun4it@gmail.com>
2022-02-19 19:10:27 +0530

    adding feedbox, comment box, actions

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ReactComponent as LikeAction } from '../assets/images/like.svg'
+import { ReactComponent as CommentAction } from '../assets/images/comment.svg'
+import { ReactComponent as ShareAction } from '../assets/images/share.svg'
+import { ReactComponent as SendAction } from '../assets/images/send.svg'
 
 const Main = () => {
   return (
@@ -29,7 +33,68 @@ const Main = () => {
 			</div>
 		</PostBox>
 		<ArticleCard>
-			Article card
+			<ArticleActor>
+				<div className="actor">
+					<a href="">
+						<img src="/images/user.svg" alt="user" />
+						<div>
+							<span className="title">Title</span>
+							<span className="info">Info</span>
+							<span className="date">Date</span>
+						</div>
+					</a>
+				</div>
+				<div className="ellipsis">
+					<a href="">
+						<img src="/images/ellipsis.svg" alt="elipsis" />
+					</a>
+				</div>
+			</ArticleActor>
+			<ArticleDescription>
+				<span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis, ullam minus tempore praesentium ea eaque soluta iure, possimus voluptatem eum iste quasi expedita dolore temporibus nemo, molestiae dolor nesciunt placeat.</span>
+			</ArticleDescription>
+			<ArticleImage>
+				<img src="/images/article-image.jpg" alt="article image" />
+			</ArticleImage>
+			<SocialCounts>
+				<li>
+					<button>
+						<img src="/images/like.svg" alt="" />
+						<img src="/images/celebrate.svg" alt="" />
+						<img src="/images/insightful.svg" alt="" />
+						<span>132</span>
+					</button>
+				</li>
+				<li>
+					<button><span>9 comments</span></button>
+				</li>
+			</SocialCounts>
+			<SocialActions>
+				<div>
+					<button>
+						<LikeAction />	
+						<span>Like</span>
+					</button>
+					<button>
+						<CommentAction />
+						<span>Comment</span>
+					</button>
+					<button>
+						<ShareAction />
+						<span>Share</span>
+					</button>
+					<button>
+						<SendAction />
+						<span>Send</span>
+					</button>
+				</div>
+			</SocialActions>
+			{/* <CommentBox>
+				<div>
+					<img className="avatar" src="/images/user.svg" alt="user" />
+					<button>Start a post</button>
+				</div>
+			</CommentBox> */}
 		</ArticleCard>
 	</Wrapper>
   )
@@ -50,6 +115,88 @@ background-color: #fff;
 
 const ArticleCard = styled(CommonCard)`
 
+`
+
+const ArticleActor = styled.div`
+display: flex;
+margin-bottom: 15px;
+.actor {
+	flex-grow: 1;
+	a {
+		text-decoration: none;
+		display: flex;
+		img {
+			width: 48px;
+			height: 48px;
+			border-radius: 50%;
+			margin-right: 15px;
+		}
+		div {
+			.title {
+				display: block;
+				font-size: 1.5rem;
+				color: rgba(0,0,0,1);
+				font-weight: 600;
+				line-height: 1;
+				margin-bottom: 0.3rem;
+			}
+			.info {
+				display: block;
+				font-size: 1.4rem;
+				color: rgba(0,0,0,0.7);
+				font-weight: 400;
+				line-height: 1;
+				margin-bottom: 0.2rem;
+			}
+			.date {
+				display: block;
+				font-size: 1.3rem;
+				color: rgba(0,0,0,0.7);
+				font-weight: 400;
+				line-height: 1;
+				margin-bottom: 0.2rem;
+			}
+		}
+	}
+}
+.ellipsis {
+	margin-top: -5px;
+	a {
+		display: inline-flex;
+		height: 32px;
+		width: 32px;
+		align-items: center;
+		justify-content: center;
+		background-color: #fff;
+		transition: 0.5s;
+		border-radius: 50%;
+		img {
+			
+		}
+
+		&:hover {
+			background-color: rgba(0,0,0,0.05)
+		}
+	}
+}
+`
+
+const ArticleDescription = styled.div`
+	span {
+		display: block;
+		font-size: 1.5rem;
+		color: rgba(0,0,0,0.9);
+		font-weight: 400;
+		line-height: 1.3;
+		margin-bottom: 15px;
+	}
+`
+
+const ArticleImage = styled.div`
+	margin: auto -15px;
+	img {
+		width: 100%;
+	}
 `
 
 const PostBox = styled(CommonCard)`
@@ -107,5 +254,78 @@ div:nth-child(2) {
 	}
 }
 `
+
+const SocialCounts = styled.ul`
+display: flex;
+align-items: center;
+justify-content: space-between;
+list-style: none;
+border-bottom: 1px solid rgba(0,0,0, 0.08);
+padding: 0.5rem 0;
+li {
+	button {
+		background-color: transparent;
+		border: none;
+		border-radius: none;
+		display: inline-flex;
+		align-items:end ;
+		cursor: pointer;
+		img {
+			width: 16px;
+			height: 16px;
+			margin-left: -5px;
+			border-radius: 50%;
+			border: 1px solid #fff;
+			background-color: #fff;
+		}
+		img:first-child() {
+			margin-left: 0;
+		}
+		span {
+			display: inline-block;
+			font-size: 1.2rem;
+			color: rgba(0,0,0,0.6);
+		}
+		&:hover {
+			text-decoration: underline;
+			color: #0a66c2;
+		}
+	}
+}
+`
+
+const SocialActions = styled.div`
+margin: 0.5rem 0 -10px;
+div {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	button {
+		cursor: pointer;
+		border: none;
+		border-radius: 5px;
+		background-color: transparent;
+		display: inline-flex;
+		align-items: center;
+		justify-content: stretch;
+		padding: 10px 10px;
+		flex: 0 1 auto;
+		span {
+			font-size: 1.35rem;
+			font-weight: 600;
+			color: rgba(0,0,0,0.4);
+		}
+		svg {
+			margin-right: 5px;
+			color: rgba(0,0,0,0.6);
+		}
+		&:hover {
+			background-color: rgba(0,0,0,0.08);
+		}
+	}
+}
+`
+
+const CommentBox = styled.div``
 
 export default Main
